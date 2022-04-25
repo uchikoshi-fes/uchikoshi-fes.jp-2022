8b4a1a488a3c0e6053190e7b94e8b1388668039c
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-04-26 07:33:19 +0900

    Add: sanitize.css を追加

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,14 @@
  * This file is licensed under the MIT License, see /LICENSE file.
  */
 
+// sanitize.css
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/assets.css";
+import "sanitize.css/typography.css";
+import "sanitize.css/system-ui.css";
+import "sanitize.css/ui-monospace.css";
+// original
 import "../styles/globals.scss";
 
 const UchikoshiFesApp = ({ Component, pageProps }) => {
