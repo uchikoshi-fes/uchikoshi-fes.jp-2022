3606d48a11d05a8cc032b43e05d6f3a7f604b03b
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-04-28 07:29:17 +0900

    Fix: OGP の description に ！ が多いのを修整

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,8 +7,8 @@ export const SITE_NAME = "第43回打越祭公式サイト";
 export const DEFAULT_TITLE = `${SITE_NAME} (浅野学園2022年度文化祭)`;
 export const DESCRIPTION =
   "中高一貫の男子校「浅野中学校・高等学校」の" +
-  "中学生、高校生、全員で作る展示の数々をどうぞ！" +
-  "東京や川崎に近い、神奈川県横浜市にて2022年9月開催予定！" +
+  "中学生、高校生、全員で作る展示の数々をどうぞ。" +
+  "東京や川崎に近い、神奈川県横浜市にて2022年9月開催予定。" +
   "史上最高の文化祭へ是非お越しください！";
 
 export const genTitle = (pageName) => `${pageName} - ${SITE_NAME}`;
