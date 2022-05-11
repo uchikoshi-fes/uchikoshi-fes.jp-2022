68e8c2efd5b72d51d231df65cae9cfb90da4f3ed
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-05-12 07:59:31 +0900

    Fix: ライセンス表記の復元

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+/*
+ * © 2022 uchikoshi-fes
+ * This file is licensed under the MIT License, see /LICENSE file.
+ */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
