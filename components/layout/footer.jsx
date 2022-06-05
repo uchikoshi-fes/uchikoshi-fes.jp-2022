a500913cd274002f78cac65791766ea940c0d268
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-06 07:37:09 +0900

    Update: フッターを整備

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -4,7 +4,34 @@
 import styles from "./footer.module.scss";
 
 const Footer = () => {
-  return <footer className={styles.footer}>フッタ</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        {/*<span>お問い合わせ</span>*/}
+        {/*<span>プライバシー</span>*/}
+        {/*<span>ライセンス</span>*/}
+        <span>
+          <a
+            href="https://www.asano.ed.jp/"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            浅野学園公式サイト
+          </a>
+        </span>
+        <span>
+          <a
+            href="https://maps.app.goo.gl/oHHMCEr89Cpeu6XEA"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            神奈川県横浜市神奈川区子安台1丁目3-1
+          </a>
+        </span>
+        <small>Copyright &copy; 2022 浅野学園生徒会</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
