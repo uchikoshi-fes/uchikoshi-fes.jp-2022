a14f815d14584f70bf269f50db63749309ec1017
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-10 17:06:49 +0900

    Update: 日数のカウントダウンを実装

--- components/countdown.jsx
+++ components/countdown.jsx
@@ -5,14 +5,26 @@ import React from "react";
 // styles
 import styles from "./countdown.module.scss";
 
+export const FES_FIRST_DAY = 1663426800000; // 2022/09/18
+export const FES_START = FES_FIRST_DAY + 9 * 3600000; // 09:00
+export const FES_END = FES_FIRST_DAY + 86400000 + 18 * 3600000; // 18:00
+
 const Countdown = () => {
   const [now, setNow] = React.useState(Date.now());
   React.useEffect(() => {
     setTimeout(() => setNow(Date.now()), 1000);
   }, [now]);
   return (
-    <div className={styles.countdown} suppressHydrationWarning>
-      {now}
+    <div className={styles.countdown}>
+      <p>
+        文化祭まで、
+        <br />
+        あと
+        <span suppressHydrationWarning>
+          {1 + Math.floor((FES_FIRST_DAY - now) / 86400000)}
+        </span>
+        日！
+      </p>
     </div>
   );
 };
