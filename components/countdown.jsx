f022a69c1e50455a69188b8e16efb2fadf5a800d
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-10 16:00:13 +0900

    Update: 1 秒毎にカウントダウンを再レンダリングできるように

--- components/countdown.jsx
+++ components/countdown.jsx
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
 
+// next
+import React from "react";
 // styles
 import styles from "./countdown.module.scss";
 
 const Countdown = () => {
-  return <div className={styles.countdown}>カウントダウン</div>;
+  const [now, setNow] = React.useState(Date.now());
+  React.useEffect(() => {
+    setTimeout(() => setNow(Date.now()), 1000);
+  }, [now]);
+  return (
+    <div className={styles.countdown} suppressHydrationWarning>
+      {now}
+    </div>
+  );
 };
 
 export default Countdown;
