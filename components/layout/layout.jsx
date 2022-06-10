ae7f770066f056d5bd7251cd19aa966d8e0acba0
Gendaineko2222 <93499945+gendaineko2222@users.noreply.github.com>
2022-06-10 14:25:31 +0000

    Refactor: 絶対パスでのインポートでエイリアスを指定

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 // components
-import Header from "components/layout/header";
-import Outline from "components/layout/outline";
-import Footer from "components/layout/footer";
+import Header from "@/components/layout/header";
+import Outline from "@/components/layout/outline";
+import Footer from "@/components/layout/footer";
 // styles
 
 const Layout = ({ children }) => {
