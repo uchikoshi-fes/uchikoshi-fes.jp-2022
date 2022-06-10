134d797d384bec925259686020d28bacd380b18f
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-10 10:28:54 +0900

    Add: カウントダウンのコンポーネントを作成

---
+++ components/countdown.jsx
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+// styles
+import styles from "./countdown.module.scss";
+
+const Countdown = () => {
+  return <div className={styles.countdown}>カウントダウン</div>;
+};
+
+export default Countdown;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@
 
 // components
 import NextSeo from "next-seo";
+import Countdown from "../components/countdown";
 // styles
 import styles from "./index.module.scss";
 
@@ -24,6 +25,7 @@ const Index = () => {
           </p>
         </div>
       </div>
+      <Countdown />
     </>
   );
 };
