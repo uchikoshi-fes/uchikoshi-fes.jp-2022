57a3e581747a251d000e71d20e31f88fbc4c7163
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-13 00:57:07 +0900

    Update: JS が実行されるまで残り日数を表示しないように
    デフォルトでビルド時の残り日数になっていて、 JS が何らかの原因で実行されなかった時に間違った情報が提供される恐れがあったため

--- components/countdown.jsx
+++ components/countdown.jsx
@@ -9,21 +9,32 @@ export const FES_FIRST_DAY = 1663426800000; // 2022/09/18
 export const FES_START = FES_FIRST_DAY + 9 * 3600000; // 09:00
 export const FES_END = FES_FIRST_DAY + 86400000 + 18 * 3600000; // 18:00
 
-const Countdown = () => {
-  const [now, setNow] = React.useState(Date.now());
+const Remaining = () => {
+  const [now, setNow] = React.useState();
   React.useEffect(() => {
     setTimeout(() => setNow(Date.now()), 1000);
   }, [now]);
+
+  if (now === undefined) return <></>;
+  else
+    return (
+      <>
+        あと
+        <span className={styles.number}>
+          {1 + Math.floor((FES_FIRST_DAY - now) / 86400000)}
+        </span>
+        日！
+      </>
+    );
+};
+
+const Countdown = () => {
   return (
     <div className={styles.countdown}>
       <div>
         文化祭開幕まで、
-        <div className={styles.remaining}>
-          あと
-          <span className={styles.number} suppressHydrationWarning>
-            {1 + Math.floor((FES_FIRST_DAY - now) / 86400000)}
-          </span>
-          日！
+        <div className={styles.remaining} suppressHydrationWarning>
+          <Remaining />
         </div>
       </div>
     </div>
