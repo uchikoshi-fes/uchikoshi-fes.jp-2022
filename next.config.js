6eb539d516b362e9766587afaf02194f68fa5413
Gendaineko2222 <93499945+gendaineko2222@users.noreply.github.com>
2022-06-17 04:05:01 +0000

    Feat: @next/mdx で MDX に対応

--- next.config.js
+++ next.config.js
@@ -3,9 +3,20 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "md", "mdx"],
   images: {
     loader: "custom",
   },
 };
 
-module.exports = nextConfig;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX(nextConfig);
