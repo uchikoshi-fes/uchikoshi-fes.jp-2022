da829159e8a14cd778db095f92786fa94137528d
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-06-23 21:08:08 +0900

    Chore: Font Awesome 導入

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,9 +12,14 @@ import "sanitize.css/assets.css";
 import "sanitize.css/typography.css";
 import "sanitize.css/system-ui.css";
 import "sanitize.css/ui-monospace.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./global.scss";
 // config
+import { config } from "@fortawesome/fontawesome-svg-core";
 import SEO from "@/next-seo.config";
+
+// why: https://fontawesome.com/docs/web/use-with/react/use-with
+config.autoAddCss = false;
 
 const UchikoshiFesApp = ({ Component, pageProps }) => {
   const router = useRouter();
