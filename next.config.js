781472153e1622a5d3a018b33ab45be5aa620c03
Mogyuchi <git@mogyuchi.com>
2022-07-09 12:38:49 +0900

    Add: next-compose-plugins 追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+const withPlugins = require("next-compose-plugins");
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,14 +11,17 @@ const nextConfig = {
   },
 };
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
+const plugins = [];
 
-module.exports = withMDX(nextConfig);
+plugins.push([
+  require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+      // If you use `MDXProvider`, uncomment the following line.
+      // providerImportSource: "@mdx-js/react",
+    },
+  }),
+]);
+module.exports = withPlugins(plugins, nextConfig);
