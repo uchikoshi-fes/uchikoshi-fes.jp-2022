f38b457c998f0213cb41c47bed12eb97f5998f25
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-07-21 15:03:34 +0900

    Add: メニューアイコンを追加

--- components/layout/menu.jsx
+++ components/layout/menu.jsx
@@ -1,7 +1,20 @@
 // SPDX-License-Identifier: MIT
 
+// components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// styles
+import styles from "./menu.module.scss";
+// icons
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 const Menu = ({ narrow }) => {
-  return <nav>{narrow ? "hamburger" : "menu"}</nav>;
+  if (narrow)
+    return (
+      <button className={styles.hamburger}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    );
+  else return <nav>Menu is here.</nav>;
 };
 
 export default Menu;
