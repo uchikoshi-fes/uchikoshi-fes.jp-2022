c1bd4d7448e6e2cd42b8440c776378f80bec90ee
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-07-22 14:15:34 +0900

    Add: スライドメニュー作成

--- components/layout/menu.jsx
+++ components/layout/menu.jsx
@@ -1,20 +1,44 @@
 // SPDX-License-Identifier: MIT
 
+// react
+import React from "react";
 // components
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "@/components/base/link";
 // styles
 import styles from "./menu.module.scss";
 // icons
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Menu = ({ narrow }) => {
-  if (narrow)
-    return (
-      <button className={styles.hamburger}>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-    );
-  else return <nav>Menu is here.</nav>;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <nav className={styles.container}>
+      {narrow ? (
+        <>
+          <button className={styles.hamburger} onClick={() => setOpen(true)}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <div
+            className={`${styles.narrow} ${open ? styles["narrow-open"] : ""}`}
+          >
+            <button
+              className={styles["narrow-closebutton"]}
+              onClick={() => setOpen(false)}
+            >
+              <FontAwesomeIcon icon={faXmark} />
+            </button>
+            <ul className={styles["narrow-content"]}>
+              {/* TODO: ここにメニュー */}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <ul className={styles.wide}>{/* TODO: ここにメニュー */}</ul>
+      )}
+    </nav>
+  );
 };
 
 export default Menu;
