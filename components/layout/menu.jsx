40595e5b577fa7f65fdd6f06095a75cdb220b0eb
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-07-25 11:51:12 +0900

    Clean: Menu を狭い画面の時と広い画面の時でコンポーネントを分ける

--- components/layout/menu.jsx
+++ components/layout/menu.jsx
@@ -10,38 +10,50 @@ import styles from "./menu.module.scss";
 // icons
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-const Menu = ({ narrow }) => {
+const MenuLinks = () => {
+  return <>{/* TODO */}</>;
+};
+
+const NarrowMenu = () => {
   const [open, setOpen] = React.useState(false);
 
   return (
+    <>
+      <button
+        title="目次を開く"
+        className={styles.hamburger}
+        onClick={() => setOpen(true)}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <div className={`${styles.narrow} ${open ? styles["narrow-open"] : ""}`}>
+        <button
+          title="目次を閉じる"
+          className={styles["narrow-closebutton"]}
+          onClick={() => setOpen(false)}
+        >
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+        <ul className={styles["narrow-content"]}>
+          <MenuLinks />
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const WideMenu = () => {
+  return (
+    <ul className={styles.wide}>
+      <MenuLinks />
+    </ul>
+  );
+};
+
+const Menu = ({ narrow }) => {
+  return (
     <nav className={styles.container}>
-      {narrow ? (
-        <>
-          <button
-            title="目次を開く"
-            className={styles.hamburger}
-            onClick={() => setOpen(true)}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </button>
-          <div
-            className={`${styles.narrow} ${open ? styles["narrow-open"] : ""}`}
-          >
-            <button
-              title="目次を閉じる"
-              className={styles["narrow-closebutton"]}
-              onClick={() => setOpen(false)}
-            >
-              <FontAwesomeIcon icon={faXmark} />
-            </button>
-            <ul className={styles["narrow-content"]}>
-              {/* TODO: ここにメニュー */}
-            </ul>
-          </div>
-        </>
-      ) : (
-        <ul className={styles.wide}>{/* TODO: ここにメニュー */}</ul>
-      )}
+      {narrow ? <NarrowMenu /> : <WideMenu />}
     </nav>
   );
 };
