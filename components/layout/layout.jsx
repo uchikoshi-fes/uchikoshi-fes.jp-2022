d09e3168131a19cb11f1541d64785b9917894c52
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-07-26 01:39:05 +0900

    Fix: メニューを開いている間ページをスクロールできなくする

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -5,15 +5,16 @@ import Header from "./header";
 import Outline from "./outline";
 import Footer from "./footer";
 // styles
+import styles from "./layout.module.scss";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
       <main>{children}</main>
       <Outline />
       <Footer />
-    </>
+    </div>
   );
 };
 
