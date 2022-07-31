abd57dd688fa0152d63dff1dcc7e74d79f027000
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-01 07:54:18 +0900

    Update: 現状のフッターを整備

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -7,42 +7,50 @@ import styles from "./footer.module.scss";
 // config
 import PACKAGE from "@/package";
 
+const Sns = () => {
+  return (
+    <div className={styles.sns}>
+      {/*<div>TODO: LINE のボタン</div>*/}
+      <div>
+        <Link
+          href="https://twitter.com/uchikoshifes?ref_src=twsrc%5Etfw"
+          className="twitter-follow-button"
+          data-size="large"
+          data-lang="ja"
+          data-show-count="false"
+        >
+          @uchikoshifesさんをフォロー
+        </Link>
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charSet="utf-8"
+        ></script>
+      </div>
+      {/*<div>TODO: Instagram のボタン</div>*/}
+      {/*<div>TODO: YouTube のボタン</div>*/}
+    </div>
+  );
+};
+
+const Others = () => {
+  return (
+    <div className={styles.others}>
+      <Link href="/contact">お問い合わせ</Link>
+      <Link href="/privacy">プライバシーポリシー</Link>
+      <Link href="/licenses">ライセンス</Link>
+      <Link href="https://www.asano.ed.jp/">浅野学園公式サイトはこちら</Link>
+      <small>v{PACKAGE.version}</small>
+      <small>Copyright &copy; 2022 浅野学園生徒会</small>
+    </div>
+  );
+};
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>
-        {/*<span>TODO: LINE のボタン</span>*/}
-        <span>
-          <Link
-            href="https://twitter.com/uchikoshifes?ref_src=twsrc%5Etfw"
-            className="twitter-follow-button"
-            data-size="large"
-            data-lang="ja"
-            data-show-count="false"
-          >
-            @uchikoshifesさんをフォロー
-          </Link>
-          <script
-            async
-            src="https://platform.twitter.com/widgets.js"
-            charSet="utf-8"
-          ></script>
-        </span>
-        {/*<span>TODO: Instagram のボタン</span>*/}
-        {/*<span>TODO: YouTube のボタン</span>*/}
-      </div>
-      <div>
-        {/*<span>お問い合わせ</span>*/}
-        {/*<span>プライバシー</span>*/}
-        {/*<span>ライセンス</span>*/}
-        <span>
-          <Link href="https://www.asano.ed.jp/">
-            浅野学園公式サイトはこちら
-          </Link>
-        </span>
-        <small>v{PACKAGE.version}</small>
-        <small>Copyright &copy; 2022 浅野学園生徒会</small>
-      </div>
+      <Sns />
+      <Others />
     </footer>
   );
 };
