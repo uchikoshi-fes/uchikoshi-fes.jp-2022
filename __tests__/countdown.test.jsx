ecebb6b4a00b331fd0cd7b02b4c28709692c88c1
りすりす/TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-16 15:03:33 +0000

    Test: テストを整理

--- __tests__/countdown.test.jsx
+++ __tests__/countdown.test.jsx
@@ -6,41 +6,57 @@ import "@testing-library/jest-dom";
 // components
 import Countdown from "@/components/countdown";
 
+const testRemaining = (
+  {
+    year = 2022,
+    month,
+    day,
+    hour = 0,
+    minute = 0,
+    second = 0,
+    millisecond = 0,
+  },
+  texts
+) => {
+  const time = new Date(
+    Date.UTC(year, month - 1, day, hour, minute, second, millisecond) -
+      9 * 3600000
+  );
+  test(
+    time.toLocaleString("ja-JP", {
+      dateStyle: "full",
+      timeStyle: "long",
+      timeZone: "Asia/Tokyo",
+    }),
+    async () => {
+      Date.now = jest.fn(() => time.getTime());
+      render(<Countdown />);
+      for (const text of texts)
+        expect(await screen.findByText(text)).toBeInTheDocument();
+    }
+  );
+};
+
 describe("Countdown", () => {
-  test("最低限のテキスト", () => {
-    render(<Countdown />);
-    expect(screen.getByText(/文化祭開幕まで/)).toBeInTheDocument();
-  });
   // 100 日前付近
-  test("06/09 23:59:00", async () => {
-    Date.now = jest.fn(() => 1654786740000);
-    render(<Countdown />);
-    expect(await screen.findByText(/101/)).toBeInTheDocument();
-    expect(await screen.findByText(/日/)).toBeInTheDocument();
-  });
-  test("06/10 00:01:00", async () => {
-    Date.now = jest.fn(() => 1654786860000);
-    render(<Countdown />);
-    expect(await screen.findByText(/100/)).toBeInTheDocument();
-    expect(await screen.findByText(/日/)).toBeInTheDocument();
-  });
-  test("06/10 12:00:00", async () => {
-    Date.now = jest.fn(() => 1654830000000);
-    render(<Countdown />);
-    expect(await screen.findByText(/100/)).toBeInTheDocument();
-    expect(await screen.findByText(/日/)).toBeInTheDocument();
-  });
-  test("06/10 23:59:00", async () => {
-    Date.now = jest.fn(() => 1654873140000);
-    render(<Countdown />);
-    expect(await screen.findByText(/100/)).toBeInTheDocument();
-    expect(await screen.findByText(/日/)).toBeInTheDocument();
-  });
-  test("06/11 00:01:00", async () => {
-    Date.now = jest.fn(() => 1654873260000);
-    render(<Countdown />);
-    expect(await screen.findByText(/99/)).toBeInTheDocument();
-    expect(await screen.findByText(/日/)).toBeInTheDocument();
-  });
-  // TODO: 24 時間以内時のテストを追加
+  testRemaining({ month: 6, day: 9, hour: 23, minute: 59, second: 59 }, [
+    /文化祭１日目開幕まで/,
+    /101/,
+  ]);
+  testRemaining({ month: 6, day: 10, hour: 0, minute: 0, second: 1 }, [
+    /文化祭１日目開幕まで/,
+    /100/,
+  ]);
+  testRemaining({ month: 6, day: 10, hour: 12, minute: 0, second: 0 }, [
+    /文化祭１日目開幕まで/,
+    /100/,
+  ]);
+  testRemaining({ month: 6, day: 10, hour: 23, minute: 59, second: 59 }, [
+    /文化祭１日目開幕まで/,
+    /100/,
+  ]);
+  testRemaining({ month: 6, day: 11, hour: 0, minute: 0, second: 1 }, [
+    /文化祭１日目開幕まで/,
+    /99/,
+  ]);
 });
