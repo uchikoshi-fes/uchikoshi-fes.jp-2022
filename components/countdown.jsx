c235bf44ffc450d225e73c46e37bb2dd582eaed4
りすりす/TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-16 14:14:55 +0000

    Clean: カウントダウンの useEffect の ESLint の Warning を無効化
    その旨もコメントで書いた！天才！

--- components/countdown.jsx
+++ components/countdown.jsx
@@ -80,21 +80,26 @@ const Countdown = () => {
   const [now, setNow] = React.useState(Date.now());
   const [intervalMs, setIntervalMs] = React.useState(1000);
   const [intervalId, setIntervalId] = React.useState();
-  React.useEffect(() => {
-    clearInterval(intervalId);
-    if (!isClient) return;
-    setIntervalId(
-      setInterval(() => {
-        setNow(Date.now());
-        // 残りが 100 秒以内になったら 250ms で更新、それ以外は 1000ms で更新
-        if (FES_TIMES.some((time) => time.start - now < 100000)) {
-          if (intervalMs > 250) setIntervalMs(250);
-        } else {
-          if (intervalMs > 1000) setIntervalMs(1000);
-        }
-      }, intervalMs)
-    );
-  }, [isClient, intervalMs]);
+  React.useEffect(
+    () => {
+      clearInterval(intervalId);
+      if (!isClient) return;
+      setIntervalId(
+        setInterval(() => {
+          setNow(Date.now());
+          // 残りが 100 秒以内になったら 250ms で更新、それ以外は 1000ms で更新
+          if (FES_TIMES.some((time) => time.start - now < 100000)) {
+            if (intervalMs > 250) setIntervalMs(250);
+          } else {
+            if (intervalMs > 1000) setIntervalMs(1000);
+          }
+        }, intervalMs)
+      );
+    },
+    // intervalId と now が変わっても更新する必要はないので、警告を出さない
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isClient, intervalMs]
+  );
 
   return (
     <div className={styles.countdown}>
