4ed5b3b7cbbdd90bc515c755a266ac0ea090dd3b
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-18 10:58:08 +0900

    Update: ファーストビューの背景に画像を入れる

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@
 
 // components
 import NextSeo from "next-seo";
+import Image from "@/components/base/image";
 import Countdown from "@/components/countdown";
 // styles
 import styles from "./index.module.scss";
@@ -11,18 +12,28 @@ const Index = () => {
     <>
       <div className={styles.top}>
         <div>
-          <h1 className={styles.title}>
-            第43回
-            <wbr />
-            打越祭
-            <wbr />
-            公式サイト
-          </h1>
-          <p className={styles.description}>
-            浅野学園2022年度文化祭
-            <br />
-            鋭意制作中！
-          </p>
+          <Image
+            src="/souichiro.jpeg"
+            alt=""
+            layout="fill"
+            className={styles["top-image"]}
+          />
+        </div>
+        <div className={styles["top-text"]}>
+          <div>
+            <h1 className={styles.title}>
+              第43回
+              <wbr />
+              打越祭
+              <wbr />
+              公式サイト
+            </h1>
+            <p className={styles.description}>
+              浅野学園2022年度文化祭
+              <br />
+              鋭意制作中！
+            </p>
+          </div>
         </div>
       </div>
       <div className={styles["countdown-container"]}>
