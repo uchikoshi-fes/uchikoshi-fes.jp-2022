6d338b5818d8672e8839dd46bb9cde0c459b02d9
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-23 21:27:01 +0900

    Add: スローガンの説明を追加

--- components/slogan.jsx
+++ components/slogan.jsx
@@ -6,7 +6,15 @@ import styles from "./slogan.module.scss";
 const Slogan = () => {
   return (
     <article className={styles.slogan}>
-      <h2>スローガン</h2>
+      <h2>
+        スローガン
+        <wbr />
+        「雲外蒼天」
+      </h2>
+      <p>
+        <>「うんがいそうてん」と読む四字熟語で、</>
+        <>困難を努力して克服すれば良いことが待っているという意味です。</>
+      </p>
     </article>
   );
 };
