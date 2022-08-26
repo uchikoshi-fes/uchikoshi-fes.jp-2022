18bf86ce5306089e2bafd80655d830813ad5f6ff
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-08-26 17:22:33 +0900

    Update: アサノラジオの番組表のデザインをいい感じに

--- components/asano-radio.jsx
+++ components/asano-radio.jsx
@@ -3,13 +3,23 @@
 // react
 import React from "react";
 // components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "@/components/base/link";
 // styles
 import styles from "./asano-radio.module.scss";
+// icons
+import {
+  faClock,
+  faRadio,
+  faUsers,
+  faComments,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 
 // 番組表データは以下の Google Spreadsheet を元に作成しています。
 // https://docs.google.com/spreadsheets/d/1RG_a3mT8D_l1l8yx_eqrapjpBuBapCX5S_D9c5lM2Bg/edit?usp=drivesdk
 // 付属の Apps Script で JSON に変換できますが、表記ブレなどの細かな修正は手動でする必要があります。
+// title を Array にした場合、それぞれの string の間に <wbr /> が挿入されます。
 const SCHEDULE = [
   {
     title: "day 1",
@@ -70,7 +80,7 @@ const SCHEDULE = [
         end: "12:30",
       },
       {
-        title: "おにさんこちら、てのなるほうへ",
+        title: ["おにさんこちら、", "てのなるほうへ"],
         type: "guests",
         start: "12:30",
         end: "12:45",
@@ -82,37 +92,37 @@ const SCHEDULE = [
         end: "13:00",
       },
       {
-        title: "フリートークwith文実長",
+        title: ["フリートーク", "with文実長"],
         type: "free",
         start: "13:05",
         end: "13:20",
       },
       {
-        title: "フリートークwith体実長",
+        title: ["フリートーク", "with体実長"],
         type: "free",
         start: "13:25",
         end: "13:40",
       },
       {
-        title: "フリートークwith生徒会長",
+        title: ["フリートーク", "with生徒会長"],
         type: "free",
         start: "13:40",
         end: "13:55",
       },
       {
-        title: "フリートークwith偉い人たち",
+        title: ["フリートーク", "with偉い人たち"],
         type: "free",
         start: "14:00",
         end: "14:30",
       },
       {
-        title: "フリートークwith文実幹部",
+        title: ["フリートーク", "with文実幹部"],
         type: "free",
         start: "14:30",
         end: "15:00",
       },
       {
-        title: "フリートークwith先生",
+        title: ["フリートーク", "with先生"],
         type: "free",
         start: "15:05",
         end: "15:20",
@@ -142,7 +152,7 @@ const SCHEDULE = [
         end: "10:20",
       },
       {
-        title: "ASET 浅野学園特撮研究会",
+        title: ["ASET ", "浅野学園特撮研究会"],
         type: "guests",
         start: "10:20",
         end: "10:35",
@@ -208,7 +218,7 @@ const SCHEDULE = [
         end: "13:55",
       },
       {
-        title: "フリートークwith文実幹部",
+        title: ["フリートーク", "with文実幹部"],
         type: "free",
         start: "14:00",
         end: "15:00",
@@ -229,33 +239,99 @@ const SCHEDULE = [
   },
 ];
 
+const timeStrToMinutes = (timeString) => {
+  const [hour, minute] = timeString.split(":");
+  return parseInt(hour) * 60 + parseInt(minute);
+};
+
 const Schedule = () => {
   return (
     <article className={styles.schedule}>
       <h3>番組表</h3>
-      {SCHEDULE.map(({ title, date, programs }) => (
-        <table border="1" key={title}>
-          <thead>
-            <tr>
-              <th colSpan="2">{title}</th>
-            </tr>
-            <tr>
-              <th>時間</th>
-              <th>番組</th>
-            </tr>
-          </thead>
-          <tbody>
-            {programs.map(({ title, type, start, end }) => (
-              <tr key={`${title}-${start}`}>
-                <td>
-                  {start} ~ {end}
-                </td>
-                <td>{title}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ))}
+      <div>
+        <div>
+          {SCHEDULE.map(({ title, date, programs }) => (
+            <table className={styles["schedule-table"]} key={title}>
+              <thead>
+                <tr>
+                  <th colSpan="2">{title}</th>
+                </tr>
+                <tr>
+                  <th className={styles["schedule-time"]}>
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className={styles["attached-icon"]}
+                    />
+                    時間
+                  </th>
+                  <th>
+                    <FontAwesomeIcon
+                      icon={faRadio}
+                      className={styles["attached-icon"]}
+                    />
+                    番組
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {programs.map(({ title, type, start, end }, index) => {
+                  const prevMinutes = timeStrToMinutes(
+                    programs[index - 1]?.end ?? start
+                  );
+                  const startMinutes = timeStrToMinutes(start);
+                  const endMinutes = timeStrToMinutes(end);
+                  return (
+                    <React.Fragment key={`${title}-${start}`}>
+                      {startMinutes !== prevMinutes && (
+                        <tr>
+                          <td
+                            colSpan="2"
+                            style={{
+                              height: `${(startMinutes - prevMinutes) / 5}em`,
+                            }}
+                          ></td>
+                        </tr>
+                      )}
+                      <tr
+                        style={{
+                          height: `${(endMinutes - startMinutes) / 5}em`,
+                        }}
+                      >
+                        <td>
+                          <div>
+                            <div className={styles["schedule-start"]}>
+                              {start}
+                            </div>
+                            <div className={styles["schedule-end"]}>{end}</div>
+                          </div>
+                        </td>
+                        <td>
+                          <FontAwesomeIcon
+                            icon={
+                              {
+                                guests: faUsers,
+                                free: faComments,
+                                special: faStar,
+                              }[type]
+                            }
+                            className={styles["attached-icon"]}
+                          />
+                          {title instanceof Array
+                            ? title.flatMap((text, i) =>
+                                // eslint-disable-next-line react/jsx-key
+                                i === 0 ? text : [<wbr />, text]
+                              )
+                            : title}
+                        </td>
+                      </tr>
+                    </React.Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          ))}
+        </div>
+      </div>
     </article>
   );
 };
