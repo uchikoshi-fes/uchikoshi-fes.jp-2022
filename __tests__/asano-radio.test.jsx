9283501907dc7d7602c8eb1f167a3831f5518bff
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-09-01 11:20:33 +0900

    Test: アサノラジオのテストで IntersectionObserver をモック

--- __tests__/asano-radio.test.jsx
+++ __tests__/asano-radio.test.jsx
@@ -6,6 +6,12 @@ import "@testing-library/jest-dom";
 // components
 import AsanoRadio from "@/components/asano-radio";
 
+window.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
 describe("AsanoRadio", () => {
   test("最低限のテキストとリンク", () => {
     render(<AsanoRadio />);
