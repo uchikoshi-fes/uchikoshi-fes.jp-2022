1fc2719eabe726f0e75e0b86f0fae660d0306e07
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-09-05 08:27:38 +0900

    Update: 予約ページ完成

--- pages/reserve.jsx
+++ pages/reserve.jsx
@@ -13,7 +13,58 @@ import Link from "@/components/base/link";
 // styles
 import styles from "./reserve.module.scss";
 
-const NOTES = ["注意事項 1", "注意事項 2", "注意事項 3"];
+const NOTES = [
+  {
+    title: "文化祭来場の注意",
+    Description: () => (
+      <ul>
+        <li className={styles.warning}>
+          来場には予約申し込みが必要で、抽選制となります。
+        </li>
+        <li>
+          当日の発熱や体調不良、療養期間中の場合はご来場をお控えください。
+        </li>
+        <li>
+          来場の際は当日の朝に検温のうえ、マスク着用での入場をお願いします。
+        </li>
+        <li>
+          来場には抽選結果に記載される内容の提示が必要です。画面の提示または、プリントアウトのご準備をお願いします。
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "予約申し込みについて",
+    Description: () => (
+      <ul>
+        <li>
+          お申し込み前に
+          <span className={styles.warning}>
+            「＠asano.ed.jp」と「＠google.com」の両方のドメインのアドレスからメールを受け取れるように確認
+          </span>
+          をお願いいたします。（キャリアの設定や迷惑メールの設定など）
+        </li>
+        <li>
+          予約フォーム入力、送信直後、登録いただいたアドレスにGoogleフォーム側より「ご記入いただきありがとうございます」のメールが届けば、予約完了となります。
+        </li>
+        <li>
+          抽選結果の連絡は「＠asano.ed.jp」のドメインのアドレスからお知らせします。
+        </li>
+        <li>
+          抽選結果のご連絡は9月13日頃を予定しています。
+          <span className={styles.warning}>
+            お電話での確認はご遠慮ください。
+          </span>
+        </li>
+        <li>
+          キャンセルのお申し出による繰り上げは行う予定です。そのため、追加の予約受付は行いません。
+        </li>
+        <li>アドレスを変えての複数のお申込みなどはご遠慮ください。</li>
+        <li>当選した場合でも他の人へ譲渡することはできません。</li>
+      </ul>
+    ),
+  },
+];
 
 const Reserve = () => {
   const [checked, setChecked] = React.useState(NOTES.map(() => false));
@@ -23,33 +74,44 @@ const Reserve = () => {
       <NextSeo title="事前予約" openGraph={{ title: "事前予約" }} />
       <article className={styles.reserve}>
         <h1>事前予約</h1>
-        {!process.env.NEXT_PUBLIC_APPLY_URL && (
-          <article className={styles.warning}>
-            <h2>現在予約は受け付けておりません</h2>
-            <p>予約は 2022/09/03 日から受け付け開始予定です。</p>
+        {!process.env.NEXT_PUBLIC_RESERVE_URL && (
+          <article>
+            <h2 className={styles.warning}>現在予約は受け付けておりません</h2>
+            <p>2022/09/05 より予約を受け付けます。</p>
           </article>
         )}
+        <p>
+          2022年9月18,19日に第43回打越祭(第２部)文化祭を開催します。
+          <br />
+          新型コロナウイルス感染症拡大防止のため、<></>
+          本年度も「来場者数の制限」を行っての開催となります。
+          <br />
+          文化祭に来場するには、下記の予約フォームよりお申し込みいただき、
+          <></>
+          抽選に参加していただく必要があります。<></>
+          申し込みに関する注意事項をよく読んでからご回答いただくよう、<></>
+          お願い申し上げます。
+        </p>
         <article>
           <h2>注意事項</h2>
-          <p>
-            予約は、以下の注意事項をよく読んでからお願いします。
-            <br />
-            予約の際には、以下の注意事項に同意したものとみなします。
-          </p>
-          {NOTES.map((note, index) => (
-            <label className={styles.note} key={index}>
-              <input
-                type="checkbox"
-                value={index}
-                checked={checked[index]}
-                onChange={(event) => {
-                  const newChecked = [...checked];
-                  newChecked[index] = event.target.checked;
-                  setChecked(newChecked);
-                }}
-              />
-              {note}
-            </label>
+          {NOTES.map(({ title, Description }, index) => (
+            <>
+              <label className={styles.note} key={index}>
+                <input
+                  type="checkbox"
+                  value={index}
+                  checked={checked[index]}
+                  className={styles.warning}
+                  onChange={(event) => {
+                    const newChecked = [...checked];
+                    newChecked[index] = event.target.checked;
+                    setChecked(newChecked);
+                  }}
+                />
+                {title}
+              </label>
+              <Description />
+            </>
           ))}
         </article>
         {process.env.NEXT_PUBLIC_RESERVE_URL && (
