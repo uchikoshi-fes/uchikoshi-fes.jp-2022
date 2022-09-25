decb43bd8ec4e5a4da3d2d059f299ca899eea09c
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-09-25 19:58:54 +0900

    Fix: ヘッダーの背景透明化が正しく機能するように

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 // react
-import React, { useState, useEffect } from "react";
+import React from "react";
 // next
 import Router from "next/router";
-// framer-motion
-import { useScroll } from "framer-motion";
 // hooks
 import { useMediaQuery } from "react-responsive";
 import useClient from "@/hooks/client";
@@ -15,31 +13,19 @@ import Menu from "./menu";
 // styles
 import styles from "./header.module.scss";
 
-const Header = ({ setScrollable }) => {
+const Header = ({ setScrollable, transparent = false }) => {
   const isClient = useClient();
   const isNarrow = useMediaQuery({ query: "(max-width: 900px)" });
-  const { scrollY } = useScroll();
-  const router = Router.useRouter();
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    return scrollY.onChange((scrollY) => {
-      setScrolled(window.innerHeight * 0.9 < scrollY);
-    });
-  }, [scrollY, router.pathname]);
 
   return (
     <header
       className={styles.header}
       style={{
-        color: router.pathname !== "/" || scrolled ? "#000" : "#fff",
-        backgroundColor:
-          router.pathname !== "/" || scrolled ? "#fdfdfd" : "rgba(0, 0, 0, 0)",
+        color: transparent ? "#fff" : "#000",
+        backgroundColor: transparent ? "rgba(0, 0, 0, 0)" : "#fdfdfd",
       }}
     >
-      {isClient && isNarrow && (
-        <Menu narrow setScrollable={setScrollable} scrolled={scrolled} />
-      )}
+      {isClient && isNarrow && <Menu narrow setScrollable={setScrollable} />}
       <div className={styles["site-name"]}>
         <Link href="/">浅野学園打越祭</Link>
       </div>
--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -2,6 +2,9 @@
 
 // react
 import React from "react";
+// hooks
+import { useRouter } from "next/router";
+import { useScroll } from "framer-motion";
 // components
 import Header from "./header";
 import Footer from "./footer";
@@ -10,14 +13,31 @@ import styles from "./layout.module.scss";
 
 const Layout = ({ children }) => {
   const [scrollable, setScrollable] = React.useState(true);
+  const [headerTransparent, setHeaderTransparent] = React.useState(null);
+  const router = useRouter();
+  const containerRef = React.useRef(null);
+  const { scrollY } = useScroll({ container: containerRef });
+  React.useEffect(() => {
+    const updateHeaderTransparent = (pagePath, pageScrollY) =>
+      setHeaderTransparent(
+        pagePath === "/" && pageScrollY <= window.innerHeight * 0.9
+      );
+    updateHeaderTransparent(router.pathname, scrollY.get());
+    return scrollY.onChange((pageScrollY) =>
+      updateHeaderTransparent(router.pathname, pageScrollY)
+    );
+  }, [router.pathname]);
 
   return (
     <div
       className={`page-container ${styles.layout} ${
         scrollable ? "" : styles["stop-scroll"]
       }`}
+      ref={containerRef}
     >
-      <Header setScrollable={setScrollable} />
+      {headerTransparent !== null && (
+        <Header setScrollable={setScrollable} transparent={headerTransparent} />
+      )}
       <main className={styles.main}>{children}</main>
       <Footer />
     </div>
